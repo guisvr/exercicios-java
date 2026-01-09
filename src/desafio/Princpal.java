8c120c7feaa20482e2072f35e998767d055473be
Guilherme Severo <gsevero.dev@gmail.com>
2026-01-09 20:58:51 -0300

    feat: implementa relatórios de estoque e reajuste em massa com ArrayList

---
+++ src/desafio/Princpal.java
@@ -0,0 +1,45 @@
+package desafio;
+import java.util.ArrayList;
+
+public class Princpal {
+    public static void main(String[] args) {
+
+        ArrayList<Produto> listaDeProdutos = new ArrayList<>();
+        
+        Produto p1 = new Produto("Cadeira", 450, 30);
+        Produto p2 = new Produto("Tapete", 200, 10);
+        Produto p3 = new Produto("Mesa", 1000, 15);
+        Produto p4 = new Produto("Sofá", 5, 2000);
+        Produto p5 = new Produto("Cortina", 13, 150);
+
+        listaDeProdutos.add(p1);
+        listaDeProdutos.add(p2);
+        listaDeProdutos.add(p3);
+        listaDeProdutos.add(p4);
+        listaDeProdutos.add(p5);
+
+        System.out.println("RELÁTÓRIO DE ESTOQUE");
+
+        double somaTotal = 0;
+
+        for (Produto p : listaDeProdutos){
+            p.exibirInformações();
+
+        somaTotal += (p.getPreco() * p.getQuantidadeEstoque());
+    }
+
+    System.out.println("A soma total de todos os itens é estoque é de R$" + somaTotal);
+
+    System.out.println("--------------\\----------------------\\----------------");
+
+    double novaSoma = 0;
+    
+    for (Produto p : listaDeProdutos){
+        p.reajustarPreco(-20);
+        p.exibirInformações();
+        novaSoma += (p.getPreco() * p.getQuantidadeEstoque());
+        }
+    
+        System.out.println("Novo valor total do estoque após o desconto: R$" + novaSoma);
+    }   
+}
---
+++ src/desafio/Produto.java
@@ -0,0 +1,59 @@
+package desafio;
+
+public class Produto {
+
+    private String nome;
+    private double preco;
+    private int quantidadeEstoque;
+
+    public Produto(String nome, double preco, int quantidadeEstoque){
+        this.nome=nome;
+        this.preco=preco;
+        this.quantidadeEstoque=quantidadeEstoque;
+    }
+
+    public void adicionarEstoque(int quantidade){
+        if(quantidade > 0){
+        this.quantidadeEstoque += quantidade;
+        System.out.println(quantidade + " itens adicionados ao estoque.");
+        }else{
+            this.quantidadeEstoque = 0;
+            System.out.println("Quantidade inválida. Estoque definido como zero.");
+        }
+    }
+
+    public void vender(int quantidade){
+        if(quantidade <= this.quantidadeEstoque){
+            this.quantidadeEstoque -= quantidade;
+            System.out.println("Venda concluida.");
+        }else{
+            System.out.println("Estoque indisponível.");
+        }
+    }
+
+        public void exibirInformações(){
+            System.out.println("Produto: " + this.nome);
+            System.out.println("Preço: R$ " + this.preco);
+            System.out.println("Quantidade em estoque: " + this.quantidadeEstoque);
+        }
+
+        public void reajustarPreco(double percentual){
+            double novoPreco = this.preco + (this.preco * (percentual/100));
+            if(novoPreco >= 0){
+                this.preco = novoPreco;
+                System.out.println("Novo preço reajustado para R$" + this.preco);
+            }else{
+                System.out.println("Preço inserido é inválido.");
+            }
+        }
+
+        public double getPreco(){
+            return this.preco;
+        }
+
+        public int getQuantidadeEstoque(){
+            return this.quantidadeEstoque;
+        }
+    }
+
+
